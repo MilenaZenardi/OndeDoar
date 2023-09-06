9ee512920163aef184921fea0bac6c992c91029f
NaiaraSuelen <na_suelen25@hotmail.com>
2023-09-05 23:11:43 -0300

    First commit - Criação do projeto Onde Doar

---
+++ src/main/java/com/ondedoar/OndedoarApplication.java
@@ -0,0 +1,13 @@
+package com.ondedoar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndedoarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndedoarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ondedoar/OndedoarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondedoar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndedoarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
